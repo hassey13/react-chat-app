4bc46e208f2ceff46128937595ba2c6ab633753e
ipc103 <iancandysemail@gmail.com>
2017-02-01 10:13:18 -0500

    beginning

--- src/components/Book.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Book (props) {
-  return <div className='something'>{props.title}</div>
-  // React.createElement
-}
-
-
-// module.exports = Book -- the browserify/node way
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Book from './components/Book'
 
-ReactDOM.render(< Book title='A River Runs Through It'/>, document.getElementById('container'))
-// const React = require('react') - this is the browserify way
